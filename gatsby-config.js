3f2ef5e085bcf9fa694fb7ab2aef1004653db1fa
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2019-04-05 11:17:07 -0300

    fix responsiveness

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `FEMUG-MG`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Página com informações do Front-end meetup group de Minas Gerais.`,
+    author: `@anacoimbrag`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,48 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from "./image"
+
+library.add(faBars)
 
 typeof window != "undefined" &&
   window.addEventListener("scroll", event => {
     const navLinks = document.getElementsByClassName("nav-link")
     const home = document.getElementById("home")
     const header = document.querySelector("header")
+    const force =
+      (document.documentElement.scrollTop >=
+        home.offsetHeight - header.offsetHeight / 2 &&
+        document.documentElement.scrollTop <=
+          home.offsetHeight * 2 - header.offsetHeight / 2) ||
+      document.documentElement.scrollTop >=
+        home.offsetHeight * 3 - header.offsetHeight / 2
+    document.getElementById("menu-icon").classList.toggle("opaque", force)
     for (const link of navLinks) {
-      link.classList.toggle(
-        "opaque",
-        (document.documentElement.scrollTop >=
-          home.offsetHeight - header.offsetHeight / 2 &&
-          document.documentElement.scrollTop <=
-            home.offsetHeight * 2 - header.offsetHeight / 2) ||
-          document.documentElement.scrollTop >=
-            home.offsetHeight * 3 - header.offsetHeight / 2
-      )
+      link.classList.toggle("opaque", force)
     }
   })
 
 const Header = ({ siteTitle }) => (
   <header
+    id="topnav"
     style={{
       position: "fixed",
       width: "100%",
-      padding: `1.45rem 1.0875rem`,
       display: "flex",
       justifyContent: "space-between",
+      flexDirection: "row-reverse",
+      flexWrap: "wrap",
     }}
   >
-    <Link to="/">
-      <Image />
-    </Link>
     <nav
+      className="navbar"
       style={{
         alignSelf: "center",
+        paddingRight: "1.0875rem",
       }}
     >
       <Link to="/#moderators" className="nav-link">
@@ -57,6 +63,18 @@ const Header = ({ siteTitle }) => (
         Código de Conduta
       </a>
     </nav>
+    <Link to="/" style={{ padding: `1.45rem 1.0875rem` }}>
+      <Image />
+    </Link>
+    <button
+      style={{ backgroundColor: "transparent", border: "none" }}
+      className="icon"
+      onClick={event => {
+        document.getElementById("topnav").classList.toggle("responsive")
+      }}
+    >
+      <FontAwesomeIcon icon="bars" id="menu-icon" color="white" />
+    </button>
   </header>
 )
 
